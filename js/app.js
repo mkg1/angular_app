90309d62cef142a3a340eaa9f5fdadb9f2057676
mkg1 <gehring.mg@gmail.com>
2016-05-11 10:17:23 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var app = angular.module('app', ['ngRoute', 'ngAnimate']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/site.html',
+    controller: 'SiteController'
+  })
+    .otherwise({
+      redirectTo: '/'
+    });
+  });
