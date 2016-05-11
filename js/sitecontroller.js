e7f8ff3b0f2dea5c5bd690d1478f23316f09760a
mkg1 <gehring.mg@gmail.com>
2016-05-11 11:44:44 -0400

    Add blue-green color change to text

--- js/sitecontroller.js
+++ js/sitecontroller.js
@@ -6,17 +6,10 @@ app.controller('SiteController', [ '$location', '$scope', '$animate', function($
   $scope.homepage = function() {
     $location.path('/');
   }
-  $scope.colorchange = function() {
-    if(color === undefined) {
-      return green;
-    } else {
-      return blue;
-    };
-  };
   $scope.setblue = function() {
-    localStorage.setItem('color', '1');
+    $scope.textType = 'blue';
   }
   $scope.setgreen = function() {
-    localStorage.setItem('color', undefined);
+    $scope.textType = 'green';
   }
 }]);
